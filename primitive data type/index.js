f130ddf561b36fbd6b424684e98a03ff14eb2b5f
Ibrahimjamal98 <135971942+Ibrahimjamal98@users.noreply.github.com>
2023-08-24 16:56:47 +0300

    Add files via upload

---
+++ primitive data type/index.js
@@ -0,0 +1,22 @@
+let x = "this is string";
+let y = 5;
+let v = true;
+let z = null;
+let e =
+
+    console.log(typeof x);
+console.log(typeof y);
+console.log(typeof v);
+console.log(typeof z);
+console.log(typeof e);
+
+//2.
+let a = "5";
+let b = 3;
+let c = a * b;
+let d = a + b;
+let e = false;
+let f = e + a;
+console.log(c); //will output 15
+console.log(d); // will output "53"
+console.log(f); //will output "05"
